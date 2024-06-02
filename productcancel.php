8f39dde48b6cb54d8983c39b6a3fa651b13accca
Phenphet <potpt1098@gmail.com>
2024-06-02 11:07:02 +0700

    add function product cancel

---
+++ productcancel.php
@@ -0,0 +1,20 @@
+<?php
+    include_once('config/Database.php');
+    include_once('service/ProductController.php');
+
+    $db = new DB();
+    $product = new ProductController($db);
+    $getValue = isset($_GET['product']) && $_GET['product'] !== "" ? $_GET['product'] : 'None';
+
+    if($getValue !== 'None'){
+        $stmt = $product->deleteProductCancel($getValue);
+        if($stmt){
+           header('Location: productremove.php');
+        }else{
+            echo 'error';
+            header('Location: productremove.php');
+        }
+    }else{
+        header('Location: productremove.php');
+    }
+?>
